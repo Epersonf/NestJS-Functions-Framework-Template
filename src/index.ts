63f1d84f66c1a3fe17b2d4c507871a1ff0b783fb
Eperson <eperson.mayrink@superopa.com>
2023-01-14 03:02:21 -0300

    NestJS functions framework template

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
+import express from 'express';
+
+const server: express.Express = express();
+
+export const createNestServer = async (expressInstance: express.Express) => {
+  const adapter = new ExpressAdapter(expressInstance);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    adapter,
+    {},
+  );
+  app.enableCors();
+  return app.init();
+};
+
+createNestServer(server)
+  .then(() => console.log('Nest Ready'))
+  .catch((err) => console.error('Nest broken', err));
+
+export { server as main };
